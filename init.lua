9ac98c029d6daa93e05f228b739ad620dc7c74e6
Carlos vinicius <viniciusdev.26@gmail.com>
2025-01-25 18:09:40 -0300

    ✨ configure shortcut to go to normal mode in tootleterminal

--- init.lua
+++ init.lua
@@ -18,3 +18,9 @@ end
 
 require "lazy_setup"
 require "polish"
+
+function _G.set_terminal_keymaps()
+  local opts = { noremap = true }
+  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+end
+vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
