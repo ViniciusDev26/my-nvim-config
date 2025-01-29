6bce4c74546978192f8e215709a1b8e345aec7ce
Carlos Vinicius <viniciusdev.26@gmail.com>
2025-01-28 22:15:39 -0300

    🔧 configure `flash-nvim` and `zellij-nav-nvim`

--- lua/community.lua
+++ lua/community.lua
@@ -7,9 +7,11 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
-  { import = "astrocommunity.colorscheme.aurora"},
-  { import = "astrocommunity.colorscheme.rose-pine"},
-  { import = "astrocommunity.colorscheme.catppuccin"},
-  { import = "astrocommunity.pack.markdown"},
-  { import = "astrocommunity.recipes.vscode-icons"}
+  { import = "astrocommunity.colorscheme.aurora" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.recipes.vscode-icons" },
+  { import = "astrocommunity.lsp.ts-error-translator-nvim" },
+  { import = "astrocommunity.pack.typescript" },
 }
